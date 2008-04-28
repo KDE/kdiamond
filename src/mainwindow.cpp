c3ec05859edc91f4a13ca769eabd5ce7ea28894d
Fela Winkelmolen <fela.kde@gmail.com>
2008-04-28 20:23:49 +0000

    Revert commits 801449, 801442, 801428, 801423, 801413.  using the name or fullname as default username is now done in KScoreDialog, doing it here actually made using the last used name not work. CCMAIL: gnushi@web.de
    svn path=/trunk/KDE/kdegames/kdiamond/; revision=802207

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -30,7 +30,6 @@
 #include <KActionCollection>
 #include <KApplication>
 #include <KConfigDialog>
-#include <KUser>
 #include <KGameDifficulty>
 #include <KGameThemeSelector>
 #include <KLocalizedString>
@@ -138,18 +137,10 @@ void MainWindow::timeIsUp()
 
 void MainWindow::gameOver()
 {
-    KUser user;
-    // Fill in default user
-    QString userName = user.property(KUser::FullName).toString();
-    if (userName.isEmpty())
-        userName = user.loginName();
-    KScoreDialog::FieldInfo scoreInfo;
-    scoreInfo[KScoreDialog::Name] = userName;
-    scoreInfo[KScoreDialog::Score].setNum(m_game->points());
     //report score
     KScoreDialog dialog(KScoreDialog::Name | KScoreDialog::Score, this);
     dialog.setConfigGroup(KGameDifficulty::levelString());
-    dialog.addScore(scoreInfo, KScoreDialog::AskName);
+    dialog.addScore(m_game->points());
     dialog.exec();
 }
 
